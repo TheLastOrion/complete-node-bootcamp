bd6e178a4146107fa32d64523f3148380ef49caa
Aldoru Gürdilek <aldorug@yahoo.com>
2023-06-27 15:06:04 +0200

    Complete Section 2
    Also with gitignore update

--- 1-node-farm/own/index.js
+++ 1-node-farm/own/index.js
@@ -1,8 +1,9 @@
-const fs = require('fs')
+const fs = require('fs');
 const http = require('http');
-const url = require ('url');
-const path = require("path");
-const querystring = require('querystring');
+const url = require('url');
+const path = require('path');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 ////////////////////////////////
 
@@ -42,95 +43,90 @@ const querystring = require('querystring');
 
 // function (err, data1) {}
 
-
 ////////////////////////////////
 
 //SERVER
-const replaceTemplate = (temp, product) =>
-{
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%PRICE%}/g, product.price)
-    output = output.replace(/{%FROM%}/g, product.from)
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%DESCRIPTION%}/g, product.description)
-    output = output.replace(/{%ID%}/g, product.id)
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
 
-    return output;
-}
-
-const data = fs.readFileSync(`${__dirname}/../starter/dev-data/data.json`, 'utf-8');
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const data = fs.readFileSync(
+  `${__dirname}/../starter/dev-data/data.json`,
+  'utf-8'
+);
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 const dataObj = JSON.parse(data);
 
+//Slugify Example
+//console.log(slugify('Fresh Avocados', {lower: true}))
+
+const slugs = dataObj.map((el) => slugify(el.productName), { lower: true });
+console.log(slugs);
 const server = http.createServer((req, res) => {
+  const baseURL = `http://${req.headers.host}`;
+  const myURL = new URL(req.url, baseURL);
 
-    const baseURL = `http://${req.headers.host}`;
-    const myURL = new URL(req.url, baseURL);
+  const pathName = myURL.pathname;
+  // const query = myURL.searchParams.get('id')
+  const queryID = myURL.searchParams.get('id');
 
-    const pathName = myURL.pathname;
-    // const query = myURL.searchParams.get('id')
-    const queryID = myURL.searchParams.get('id');
+  //console.log(baseURL+req.url    );
+  //console.log(myURL.pathname);
+  // const pathName = myURL.pathname;
+  // const query = myURL.search;
+  console.log(
+    `baseURL: ${baseURL}\nmyUrl: ${myURL}\npathName: ${pathName}\nquery: ${queryID}`
+  );
+  // Overview page
+  if (pathName === '/' || pathName === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('%PRODUCT_CARDS%', cardsHtml);
+    console.log(cardsHtml);
+    // res.end(tempOverview);
+    res.end(output);
+  }
 
-    //console.log(baseURL+req.url    );
-    //console.log(myURL.pathname);
-    // const pathName = myURL.pathname;
-    // const query = myURL.search;
-     console.log(`baseURL: ${baseURL}\nmyUrl: ${myURL}\npathName: ${pathName}\nquery: ${queryID}`)
-    // Overview page
-    if(pathName === '/' || pathName ===  '/overview') {
-        res.writeHead(200, {'Content-type': 'text/html'});
-        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-        const output = tempOverview.replace('%PRODUCT_CARDS%', cardsHtml);
-        console.log(cardsHtml);
-        // res.end(tempOverview);
-        res.end(output);
-    }
+  // Product page
+  else if (pathName === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-    // Product page
-    else if(pathName === '/product')
-    {
-        res.writeHead(200, {'Content-type': 'text/html'});
+    const product = dataObj[queryID];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+    // res.end('This is the PRODUCT');
+  }
 
-        const product = dataObj[queryID];
-        const output = replaceTemplate(tempProduct, product);
-        res.end(output);
-        // res.end('This is the PRODUCT');
+  // API
+  else if (pathName === '/api') {
+    // fs.readFile(`${__dirname}/../starter/dev-data/data.json`, 'utf-8', (err, data) => {
+    //     const productData = JSON.parse(data);
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    // console.log(dataObj);
 
-    }
+    res.end(data);
 
-    // API
-    else if(pathName === '/api')
-    {
-        // fs.readFile(`${__dirname}/../starter/dev-data/data.json`, 'utf-8', (err, data) => {
-        //     const productData = JSON.parse(data);
-            res.writeHead(200, {'Content-type': 'application/json'});
-            // console.log(dataObj);
-
-            res.end(data);
-
-        // });
-
-    }
-    else
-    {
-
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'hello-world'
-        });
-        res.end('Page not found!');
-    }
-
-
-    //res.end('Hello from the server!');
+    // });
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('Page not found!');
+  }
+  //res.end('Hello from the server!');
 });
 
-
-server.listen(8000, '127.0.0.1', ()=> {
-    console.log('Listening to requests on port 8000');
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
 });
