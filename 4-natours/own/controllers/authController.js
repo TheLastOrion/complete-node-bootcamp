327765e653d1531651585828daeb2c2a9c57bc99
Aldoru Gürdilek <aldorug@yahoo.com>
2023-07-13 15:50:27 +0200

    Section 10 mid commit

---
+++ 4-natours/own/controllers/authController.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.signup = catchAsync(async (req, res, next) => {
+  //Really bad practice to use this since everybody can masquerade as an admin when info is put in the body
+  //const newUser = await User.create(req.body); //User.save can be also used.
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+});
--- 4-natours/own/controllers/errorController.js
+++ 4-natours/own/controllers/errorController.js
@@ -40,11 +40,12 @@ const handleDuplicateFieldsDB = (err) => {
 };
 
 const handleValidationErrorDB = (err) => {
-  const errors = Object.values(err.errors).map(el => el.message);
+  const errors = Object.values(err.errors).map((el) => el.message);
   const message = `Invalid input data. ${errors.join('. ')} `;
 
   return new AppError(message, 400);
-}
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
