b26f0927e9967867325905675e0eea3d737a8e56
Aldoru Gürdilek <aldorug@yahoo.com>
2023-08-03 11:06:21 +0200

    Section 12 Leaflet Commit
    Used leaflet instead of MapBox, as it started to require credit card information on registration, just like google maps.

---
+++ 4-natours/own/public/js/mapbox.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+// ESLint is configured for NodeJS and not for javascript
+console.log('hello from the client side :D');
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
